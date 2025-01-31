e91043a5dd7a78d233867e322e49854643a4874a
tktesting-for-business <taka3chijp@gmail.com>
2025-01-31 16:56:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
-import pandas as pd
-df = pd.DataFrame({'col1': [1,2,3]})
-df
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+fig
