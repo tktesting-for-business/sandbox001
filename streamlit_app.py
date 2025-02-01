991978d9f2f89dbd4a18b57faef23413a0df4ff3
tktesting-for-business <taka3chijp@gmail.com>
2025-02-02 08:59:47 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,12 @@ import numpy as np
 # import seaborn as sns
 # import matplotlib.pyplot as plt
 
+# from PIL import Image
+
 df = pd.DataFrame({'A': range(5),
                    'B': [x**2 for x in range(5)],
                    'C': [x**3 for x in range(5)]})
 
-st.write(df)
+df_corr = df.corr()
+ 
+st.write(type(df_corr))
