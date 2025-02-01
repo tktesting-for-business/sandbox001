8d17379b973c1456112c693ca9d72e4f0e31082f
tktesting-for-business <taka3chijp@gmail.com>
2025-02-02 08:57:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # import seaborn as sns
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 df = pd.DataFrame({'A': range(5),
                    'B': [x**2 for x in range(5)],
