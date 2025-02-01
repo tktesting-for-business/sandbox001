165a481aa5d11f6e370026e645791887b71b5197
tktesting-for-business <taka3chijp@gmail.com>
2025-02-02 08:48:37 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,15 @@
 import streamlit as st
-import pandas as pd
 
-def load_iris_data():
-    """
-    データ読み込み, cacheにして最適化を行う
-    """
-    iris_data = load_iris()
-    df = pd.DataFrame(iris_data.data, columns=iris_data.feature_names)
-    labels = iris_data.target_names[iris_data.target]
-    return df, labels
+col1, col2, col3 = st.columns(3)
 
-st.write('### アイリスデータを見ていく')
-df, labels = load_iris_data()
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
 
-def show_heatmap(df):
-    """
-    各特徴の相関ヒートマップをみる
-    """
-    fig, ax = plt.subplots(figsize=(10,10))
-    sns.heatmap(df.corr(), annot=True, ax=ax)
-    st.pyplot(fig)
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
 
-st.write('特徴ごとの相関のHeatMap表示')
-show_heatmap(df)
-
-st.write('DataFrameの表示')
-st.write(df)
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
