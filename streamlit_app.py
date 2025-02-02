07f29fb77f148a335ca548a969eac2934668e16e
tktesting-for-business <taka3chijp@gmail.com>
2025-02-02 09:33:09 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,14 @@ import numpy as np
 # import seaborn as sns
 import matplotlib.pyplot as plt
 
-# from PIL import Image
-
 df = pd.DataFrame({'A': range(5),
                    'B': [x**2 for x in range(5)],
                    'C': [x**3 for x in range(5)]})
  
-st.write(df)
 
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
 
 st.pyplot(fig)
+st.write(df)
