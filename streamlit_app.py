973e98cf8b24c992eb6ca71e59c2ae165550ca4e
tktesting-for-business <taka3chijp@gmail.com>
2025-03-02 07:47:26 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from markitdown import MarkItDown
 
 md = MarkItDown()
 result = md.convert("challenge_dh7th_en.pdf")
-html_text = convert_cid_string(result.text_content, "html_entity")
+html_text = convert_cid_string(result.text_content, "unicode_literal")
 st.write(html_text)
 
 ########################################################
