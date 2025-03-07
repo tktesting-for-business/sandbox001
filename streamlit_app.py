becde15cf716ce245c0268ef599810fdcc792f4e
tktesting-for-business <taka3chijp@gmail.com>
2025-03-07 13:39:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pymupdf4llm
 
 # プログラム開始時にPDFファイルのパスを変数に指定
 pdf_file_path = "gazou.pdf"  # ここにPDFファイルのパスを指定
