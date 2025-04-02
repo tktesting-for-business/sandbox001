e70571f4348a6304055b6f0c8763bd0c2ee3a9e2
tktesting-for-business <taka3chijp@gmail.com>
2025-04-03 05:56:09 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ Net_Income =income_stmt_items("Net Income")
 
 df_output = pd.concat([Total_Revenue, Gross_Profit,Operating_Income,Pretax_Income,Net_Income], axis=1) # axis=1 で列方向に結合
 st.write(df_output.T)
-st.line_chart(df_output,x)
+st.line_chart(df_output,)
 
 # キャッシュフロー
 #cash_flow = ticker_info.cash_flow
