805addafce1b3a0c24e4186ee0a51299c79e8e46
tktesting-for-business <taka3chijp@gmail.com>
2025-04-04 07:52:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import pymupdf4llm
 
 st.set_page_config(layout="wide")
 st.title("Analyzing financial statements")
-st.write("This is a testing site used yfinance API")
+st.write("This is a testing site with yfinance API")
 with st.sidebar:
     st.title("sidebar title")
     st.button("hello")
