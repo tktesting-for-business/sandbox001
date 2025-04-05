8f5579d91e4cedd5d99702d7805816949c24cdc5
tktesting-for-business <taka3chijp@gmail.com>
2025-04-05 18:16:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -150,8 +150,9 @@ with col1:
     # データフレームを作成
     data = {
         "date": [df_output.iat[1, 1]],
-        "Total Revenue": [[df_output.iat[1, 2,]]
+        "Total Revenue": [[df_output.iat[1, 2]]
     }
+
     df = pd.DataFrame(data)
     
     # BarChartColumnで列をカスタマイズ
