db9d07adf0a3152d479c8e3fe7293134caccb53c
tktesting-for-business <taka3chijp@gmail.com>
2025-04-05 17:47:04 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,35 @@ with st.sidebar:
     st.text("hello world")
     st.divider()
     st.radio("fruits",["apple","orange","melon"])
-    
+
+
+
+data_df = pd.DataFrame(
+    {
+        "sales": [
+            [0, 4, 26, 80, 100, 40],
+            [80, 20, 80, 35, 40, 100],
+            [10, 20, 80, 80, 70, 0],
+            [10, 100, 20, 100, 30, 100],
+        ],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "sales": st.column_config.LineChartColumn(
+            "Sales (last 6 months)",
+            width="medium",
+            help="The sales volume in the last 6 months",
+            y_min=0,
+            y_max=100,
+         ),
+    },
+    hide_index=True,
+)
+
+
 ########################################################
 import yfinance as yf
 from streamlit.column_config import BarChartColumn
